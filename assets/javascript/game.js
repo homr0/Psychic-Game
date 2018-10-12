a83054e8a68bd43534a1077e6a7498008e6bad77
Rebecca Hom <rebecca.hom001@gmail.com>
2018-10-11 20:35:44 -0700

    added in missing letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 // Letters that can be guessed
 var letters = [
-    'a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'
+    'a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
 
 // Random letter function
